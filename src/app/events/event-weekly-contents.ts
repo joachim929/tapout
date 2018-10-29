91ea297e5745bcf181e702cbc7a5f8859b0594ba
Joachim Urquhart <joachim_urquhart@hotmail.co.uk>
2018-10-29 23:21:46 +0100

    Menu and events update

--- src/app/events/event-weekly-contents.ts
+++ src/app/events/event-weekly-contents.ts
@@ -6,35 +6,62 @@ import {WeeklyEventObject} from './event-content-object';
 export const EVENTWEEKLYITEMS: WeeklyEventObject[] = [
     {
         eventID: 1,
-        eventSubHeading: 'Daily Happy Hour',
-        eventTime: new Date('July 23, 2018 18:00'),
-        eventDescription: 'Buy one Cocktail, get one free from 4pm-7pm'
+        eventSubHeading: 'Check-in Mondays',
+        eventTime: new Date('October 29, 2018 18:00'),
+        eventDescription: 'Get 20% off on all food, if you check into Tap Out - ' +
+        'Eatery and Bar and tag 5 people in your post'
     },
     {
         eventID: 2,
-        eventSubHeading: 'Burgers and Beers, Mondays',
-        eventTime: new Date('July 23, 2018 18:00'),
-        eventDescription: 'Buy any Burger and get a free Tiger Beer from 5pm-9pm'
+        eventSubHeading: 'Rooster Night - Every Tuesday',
+        eventTime: new Date('October 30, 2018 18:00'),
+        eventDescription: 'We are thinking only Ga (chicken) and Bia (Rooster beers)\n' +
+        '6x wings any style, 1 medium Rooster bia - 130K ₫\n' +
+        '12x wings any style, 1 pint Rooster bia - 210K ₫\n ' +
+        '24x wings any style, 4x medium Rooster bia 350K ₫\n' +
+        'Buy 2 get the 3rd free on Rooster beers'
     },
     {
         eventID: 3,
-        eventSubHeading: 'Rooster Night, Tuesdays',
-        eventTime: new Date('July 24, 2018 22:00'),
-        eventDescription: '6x wings any style, 1 medium Rooster bia - 130K ₫\n' +
-        '12x wings any style, 1 pint Rooster bia - 210K ₫\n ' +
-        '24x wings any style, 4x medium Rooster bia 350K ₫\n' +
-        ' Buy 2 get the 3rd FREE on Rooster Bia\'s all night'
+        eventSubHeading: 'Wine Wednesdays',
+        eventTime: new Date('October 31, 2018 16:00'),
+        eventDescription: 'No need to \'WINE\' about how bad your week is going,' +
+        ' just get to Tap Out and indulge yourself in 50% off Wine by the glass\n' +
+        'Whether it is white wines to cool off in the heat in, or if it is a ' +
+        'lovely red with some of your meaty dishes\n' +
+        '4pm-8pm (wine by the glass)'
     },
     {
         eventID: 4,
-        eventSubHeading: 'Why Wine? Wednesdays',
-        eventTime: new Date('July 25, 2018 22:00'),
-        eventDescription: '50% on all house wines by the glass till 8pm'
+        eventSubHeading: 'Thursdays - Cocktail Night',
+        eventTime: new Date('November 1, 2018 16:00'),
+        eventDescription: 'Buy 1 get 1 free on cocktails 4pm-8pm\n' +
+        'Buy 2 get the 3rd free 8pm-10pm\n' +
+        'Come and take advantage of a great happy hour every Thursday,' +
+        ' on all our signature cocktails.\n' +
+        'Cocktails ranging from celery sours, to some more quirky ones'
     },
     {
         eventID: 5,
-        eventSubHeading: 'Tap that Keg Thursdays',
-        eventTime: new Date('July 26, 2018 22:00'),
-        eventDescription: 'Half a rack of ribs, choice of two sides, and free flow Tiger draft!!!'
+        eventSubHeading: 'Friday Night - Steak and Date Night',
+        eventTime: new Date('November 2, 2018 22:00'),
+        eventDescription: '250g Australian grain fed sirloin steak, plus 1 side,' +
+        ' plus 1 glass of Red (Carbenette Sauvignon) - 300K ₫\n' +
+        'Perfect deal for you and your date'
+    },
+    {
+        eventID: 6,
+        eventSubHeading: 'Saturday Night - Oyster Night',
+        eventTime: new Date('November 3, 2018 22:00'),
+        eventDescription: 'Every Saturday we have a special for all those Oyster lovers out there,' +
+        ' whether it is fresh oysters, grilled with cheese oysters, or our very own special smoked oysters'
+    },
+    {
+        eventID: 7,
+        eventSubHeading: 'Crafty Sundays',
+        eventTime: new Date('November 4, 2018 15:00'),
+        eventDescription: 'Buy 2 get the 3rd free on all craft beers rom 3pm-7pm\n' +
+        'Bar snacks and other food deals available also\n' +
+        'Ribs and a free Te Te craft beer deal'
     }
 ];
--- src/app/menu/menu-drink-items.ts
+++ src/app/menu/menu-drink-items.ts
@@ -8,50 +8,50 @@ export const DRINKITEMS: DrinkItem[] = [
         drinkCategory: [
             {
                 drinkID: 1,
-                descriptor: 'East West Triple IPA',
-                price: '110K ',
-                description: 'IBU 75 - ABV 11.0'
+                descriptor: 'East West Far East IPA',
+                price: '95K',
+                description: 'IBU 54 - ABV 6.7%'
             },
             {
                 drinkID: 2,
-                descriptor: 'East West Pacific Pilsner',
-                price: '90K',
-                description: 'IBU 22 – ABV 5.0'
+                descriptor: 'East West Pale Ale',
+                price: '95K',
+                description: 'IBU 32 - ABV 6.0%'
             },
             {
                 drinkID: 3,
                 descriptor: 'Pasteur Street Jasmin IPA',
-                price: '90K',
+                price: '95K',
                 description: 'IBU 50 - ABV 6.5%'
             },
             {
                 drinkID: 4,
                 descriptor: 'Pasteur Street Passion Fruit Wheat Ale',
-                price: '90K',
+                price: '95K',
                 description: 'IBU 15 - ABV 4.0%'
             },
             {
                 drinkID: 5,
                 descriptor: 'Heart of Darkness Loose Rivet New England IPA',
-                price: '95K',
+                price: '100K',
                 description: 'IBU 59 - ABV 7.5%'
             },
             {
                 drinkID: 6,
-                descriptor: 'Heart of Darkness Dream Alone Pale Ale',
-                price: '90K',
-                description: 'IBU 37 - ABV 5.7%'
+                descriptor: 'Heart of Darkness Mexican Pilsner',
+                price: '95K',
+                description: 'IBU 24 - ABV 4.2%'
             },
             {
                 drinkID: 7,
                 descriptor: 'Te Te White Ale',
-                price: '85K',
+                price: '90K',
                 description: 'IBU 19 - ABV 5.5%'
             },
             {
                 drinkID: 8,
                 descriptor: 'Hanoi Cider',
-                price: '100K',
+                price: '105K',
                 description: 'ABV 6.0%'
             },
             {
@@ -87,6 +87,18 @@ export const DRINKITEMS: DrinkItem[] = [
                 descriptor: 'Desperado Bottle',
                 price: '60K',
                 description: 'ABV 5.9%'
+            },
+            {
+                drinkID: 14,
+                descriptor: 'Heineken Bottle',
+                price: '60K',
+                description: 'ABV 5.0%'
+            },
+            {
+                drinkID: 13,
+                descriptor: 'Amstel Bottle',
+                price: '60K',
+                description: 'ABV 4.6%'
             }
         ], drinkCategoryID: 2, drinkCategoryName: 'Bottles'
     },
@@ -94,13 +106,13 @@ export const DRINKITEMS: DrinkItem[] = [
         drinkCategory: [
             {
                 drinkID: 14,
-                descriptor: 'Modern Belgian Dark',
+                descriptor: 'East West Modern Belgian Dark',
                 price: '200K',
                 description: '500ml - IBU 23 - ABV 8.1%'
             },
             {
                 drinkID: 15,
-                descriptor: 'Independence Stout',
+                descriptor: 'East West Independence Stout',
                 price: '220K',
                 description: '500ml - IBU 68 - ABV 12%'
             }
@@ -127,41 +139,123 @@ export const DRINKITEMS: DrinkItem[] = [
             {
                 drinkID: 18,
                 descriptor: 'Celery Sour',
-                price: '120K',
-                description: 'Gin, celery & lime juice, sugar syrup, egg whites, bitters'
+                price: '130K',
+                description: 'Gin, celery & lime juice, sugar syrup, egg whites, bitters.'
             },
             {
                 drinkID: 19,
-                descriptor: 'Tropical Climax',
-                price: '150K',
-                description: 'Light rum, dark rum, apricot brandy, triple sec, pineapple, lime & fresh passion fruit juice.'
+                descriptor: 'Espresso Martini',
+                price: '130K',
+                description: 'Vodka, Kahlua, Baileys, Vietnamese café, vanilla.'
             },
             {
                 drinkID: 20,
-                descriptor: 'Coronita',
+                descriptor: 'Deep South Peach Tea',
+                price: '140K',
+                description: 'Vodka, peach liqueur, peach puree,lime juice, bitters, earl grey honey tea.'
+            },
+            {
+                drinkID: 21,
+                descriptor: 'The Sour Honi',
+                price: '140K',
+                description: 'Smoked pineapple & char grilled banana infused rum, banana liqueur, lime & pineapple' +
+                ' reduction, egg white.'
+            },
+            {
+                drinkID: 22,
+                descriptor: 'Long Island',
+                price: '140K',
+                description: 'Vodka, rum, gin, tequila, triple sec, sugar syrup, lime juice, coke.'
+            },
+            {
+                drinkID: 23,
+                descriptor: 'Tap Out Blues',
+                price: '140K',
+                description: 'Rum, Blue Curacao, lime, sugar syrup.'
+            },
+            {
+                drinkID: 19,
+                descriptor: 'Linh\'s Bourban',
+                price: '140K',
+                description: 'Bourbon Whiskey, Crème de Cassis, lime, sugar syrup, bitters.'
+            },
+            {
+                drinkID: 20,
+                descriptor: 'Perfect Lady',
+                price: '150K',
+                description: 'Bombay gin, Peach Liquor, lime juice, egg whites.'
+            },
+            {
+                drinkID: 21,
+                descriptor: 'Cherry Blossom',
+                price: '150K',
+                description: 'Gin, Cherry Liquor, Dom Benedictine, lime juice, 5 spice and egg whites.'
+            },
+            {
+                drinkID: 22,
+                descriptor: 'Pisco & Pineapple Meringee',
+                price: '150K',
+                description: 'Pisco, Pineapple juice, Pineapple reduction, lime juice, egg whites, sugar.'
+            },
+            {
+                drinkID: 23,
+                descriptor: 'The Forgotten Lady',
+                price: '160K',
+                description: 'Rum, Apricot brandy, Mango sorbet, lime juice, bitters.'
+            },
+            {
+                drinkID: 21,
+                descriptor: 'Tropical Climax',
+                price: '160K',
+                description: 'Light rum, dark rum, apricot brandy, triple sec, pineapple, lime & fresh passion fruit juice.'
+            },
+            {
+                drinkID: 22,
+                descriptor: 'Corontina',
                 price: '180K',
                 description: 'Double shot tequila, triple sec, lime & passion fruit juice blended with ice,' +
                 ' served in a large cocktail glass with a mini corona.'
             },
             {
-                drinkID: 21,
-                descriptor: 'Espresso Martini',
-                price: '120K',
-                description: 'Vodka, Kahlua, Baileys, Vietnamese café, vanilla.'
-            },
-            {
-                drinkID: 22,
-                descriptor: 'Deep South Peach Tea',
-                price: '130K',
-                description: 'Vodka, peach liqueur, peach puree, lime juice, bitters, earl grey honey tea'
-            },
-            {
                 drinkID: 23,
-                descriptor: 'Long Island',
-                price: '120K',
-                description: 'Vodka, rum, gin, tequila, triple sec, sugar syrup, lime juice, coke.'
+                descriptor: 'Tito\s Bloody Mary',
+                price: '185K',
+                description: 'Tito’s Vodka, Horseradish, lime juice, Tabasco, Worcestershire sauce, kosher salt, ground ' +
+                'pepper in Tomato juice and a slice of bacon.'
             }
         ], drinkCategoryID: 5, drinkCategoryName: 'Cocktails'
+    },
+    {
+        drinkCategory: [
+            {
+                drinkID: 16,
+                descriptor: '4x Beer Flight - Choice of 4 Craft Beers',
+                price: '250K',
+                description: 'false'
+            },
+            {
+                drinkID: 17,
+                descriptor: '8x Beer Flight - Choice of 8 Craft Beers',
+                price: '490K',
+                description: 'false'
+            }
+        ], drinkCategoryID: 9, drinkCategoryName: 'Flights'
+    },
+    {
+        drinkCategory: [
+            {
+                drinkID: 16,
+                descriptor: 'Surf N Turf Flight',
+                price: '350K',
+                description: '4x Craft beers of your choice, along with 2x chicken wings, prawn skewer and fried oysters.'
+            },
+            {
+                drinkID: 17,
+                descriptor: 'Cluck & Moo Flight',
+                price: '330K',
+                description: '4x Craft beers of your choice, along with 2x chicken wings, pad thai chicken skewer and beef skewer'
+            }
+        ], drinkCategoryID: 10, drinkCategoryName: 'Flights & Bites'
     },
     {
         drinkCategory: [
@@ -179,7 +273,7 @@ export const DRINKITEMS: DrinkItem[] = [
             },
             {
                 drinkID: 26,
-                descriptor: 'Ribshack – Pinnotage Shiraz – South Africa 2016',
+                descriptor: 'Ribshack – Pinnotage/Merlot – South Africa 2016',
                 price: '125K/680K',
                 description: 'Glass/Bottle'
             },
@@ -202,12 +296,6 @@ export const DRINKITEMS: DrinkItem[] = [
             {
                 drinkID: 29,
                 descriptor: 'Casa Subercaseaux – Sauvignon Blanc – Chile 2017',
-                price: '95K/420K',
-                description: 'Glass/Bottle'
-            },
-            {
-                drinkID: 30,
-                descriptor: 'Woolshed - Sauvignon Blanc - Australia 2017',
                 price: '95K/420K',
                 description: 'Glass/Bottle'
             },
@@ -240,5 +328,51 @@ export const DRINKITEMS: DrinkItem[] = [
                 description: 'Bottle'
             }
         ], drinkCategoryID: 8, drinkCategoryName: 'Rose and Prosecco'
+    },
+    {
+        drinkCategory: [
+            {
+                drinkID: 33,
+                descriptor: 'Fresh Lime Juice',
+                price: '40K',
+                description: 'false'
+            },
+            {
+                drinkID: 34,
+                descriptor: 'Fresh Passion Fruit Juice',
+                price: '50K',
+                description: 'false'
+            },
+            {
+                drinkID: 33,
+                descriptor: 'Fresh Pineapple Juice',
+                price: '60K',
+                description: 'false'
+            },
+            {
+                drinkID: 34,
+                descriptor: 'Fresh Orange Juice',
+                price: '60K',
+                description: 'false'
+            },
+            {
+                drinkID: 34,
+                descriptor: 'Mixed Juice',
+                price: '70K',
+                description: 'false'
+            },
+            {
+                drinkID: 33,
+                descriptor: 'Black Vietnamese Ice Coffee',
+                price: '50K',
+                description: 'false'
+            },
+            {
+                drinkID: 34,
+                descriptor: 'Vietnamese Ice Coffee Fresh/Sweet Milk',
+                price: '60K',
+                description: 'false'
+            }
+        ], drinkCategoryID: 11, drinkCategoryName: 'Juices & Coffee'
     }
 ];
