50a6d4f857b9378bc77d0c3df176def0fad5f806
joachim929 <joachim_urquhart@hotmail.co.uk>
2019-02-05 13:32:12 +0100

    fluff

--- src/app/guest-center/pages/menu/menu.component.ts
+++ src/app/guest-center/pages/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FOODITEMS} from './menu-food-items';
+import {FOODITEMS, TETITEMS} from './menu-food-items';
 import {DRINKITEMS} from './menu-drink-items';
 
 @Component({
@@ -13,10 +13,24 @@ export class MenuComponent implements OnInit {
     drinkItems = DRINKITEMS;
     vietnameseDong = '&#8363';
 
+    tetEndDate = new Date('February 11 2019');
+    tetEventToggle: boolean;
+    tetItems;
+
     constructor() {
     }
 
     ngOnInit() {
+        const now = new Date();
+        this.tetEventToggle = this.tetEndDate > now;
+        console.log(this.tetEventToggle);
+        this.tetEvent();
+    }
+
+    tetEvent() {
+        if (this.tetEventToggle === true) {
+            this.tetItems = TETITEMS;
+        }
     }
 
 
