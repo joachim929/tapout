6151420cf7a0a845d6d8aff50a6f74f4d6ceb940
joachim929 <joachim_urquhart@hotmail.co.uk>
2019-02-25 16:57:55 +0100

    Opening hours change"

--- src/app/guest-center/pages/contact/contact.component.ts
+++ src/app/guest-center/pages/contact/contact.component.ts
@@ -12,19 +12,6 @@ export class ContactComponent implements OnInit {
     facebook: string;
     instagram: string;
 
-    normalHours = '11:00-Late';
-    specialHours = '15:00-Late';
-    endWeek = new Date('February 24 2019');
-
-    get openingHours(): string {
-        const now = new Date();
-        if (now > this.endWeek) {
-            return this.normalHours;
-        } else {
-            return this.specialHours;
-        }
-    }
-
     constructor(private externalLinksService: ExternalLinksService) {
     }
 
